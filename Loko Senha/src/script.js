aa993d6d8e160305e1b12f27ea059d78c301693b
Lokost <gabriel.zsloko@gmail.com>
2023-02-01 23:40:46 -0300

    nova página inicial e loko senha

---
+++ Loko Senha/src/script.js
@@ -0,0 +1,22 @@
+function jogar() {
+    // TODO: Ainda ver como vou fazer a junção do módulo com o script.   
+}
+
+// JavaScript atualmente não precisa mais do ";"!!!! Isso me ajuda e ao mesmo tempo nem tanto, acostumei com Kotlin, mas estou lascado ao programar em java!
+function aviso() {
+    alert('Seja bem vindo!')
+    alert('O jogo ainda está sendo desenvolvido!')
+    alert('Aguarde a próxima atualização!')
+}
+
+function voltar() {
+    location.href = './index.html'
+}
+
+function jogar() {
+    location.href = './jogo.html'
+}
+
+function download() {
+    alert('Ainda em desenvolvimento!')
+}
