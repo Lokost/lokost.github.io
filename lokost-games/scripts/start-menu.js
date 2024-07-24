000aaaf5571a52280833ef8c5d5945fef77df9d9
Gabriel Gomes <51063660+Lokost@users.noreply.github.com>
2024-07-24 15:50:43 -0300

    Update start-menu.js

--- lokost-games/scripts/start-menu.js
+++ lokost-games/scripts/start-menu.js
@@ -1,8 +1,11 @@
-import data from "../contents.json" assert { type: "json" };
+var data;
 import { fileType } from "./file_types.js";
 import { open_file } from "./app-controller.js";
 
 const start_items = data.startMenu;
+
+const req = new Request("../contents.json", {headers: {"Content-Header": "application/JSON"}});
+await fetch(req).then(e => e.json()).then(info => data = info);
 
 function startShortcut(file) {
   const shortcut = document.createElement("div");
