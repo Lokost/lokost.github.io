d53a96c35edef939999793df2aef6460a71a8b69
Gabriel Gomes <51063660+Lokost@users.noreply.github.com>
2024-07-29 01:40:11 -0300

    More some things doing better

--- freelancer/scripts/main.js
+++ freelancer/scripts/main.js
@@ -1,17 +1,60 @@
 const toggleMenuButton = document.querySelector(".menu-toggle span");
 const menu = document.querySelector(".side-menu");
+const menuBackdrop = document.querySelector(".backdrop");
+let showMenu = false;
 
 function toggle_menu() {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
+  showMenu = !showMenu;
+  console.log(showMenu);
+
+  if (showMenu) {
     menu.style.display = "block";
+    menu.classList.add("enter");
+    menu.onanimationend = () => menu.classList.remove("enter");
+
+    menuBackdrop.style.display = "block";
+    menuBackdrop.classList.add("enter");
+    menuBackdrop.onanimationend = () => menuBackdrop.classList.remove("enter");
+  } else {
+    menu.classList.add("close");
+    menu.onanimationend = () => {
+      menu.classList.remove("close");
+      menu.style.display = "none";
+    };
+
+    menuBackdrop.classList.add("close");
+    menuBackdrop.onanimationend = () => {
+      menuBackdrop.classList.remove("close");
+      menuBackdrop.style.display = "none";
+    };
   }
 }
 
 toggleMenuButton.addEventListener("click", toggle_menu);
 document.onclick = (e) => {
-  if (!menu.contains(e.target) && !toggleMenuButton.contains(e.target)) {
-    menu.style.display = "none";
+  if (
+    !menu.contains(e.target) &&
+    !toggleMenuButton.contains(e.target) &&
+    showMenu
+  ) {
+    toggle_menu();
   }
 };
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.5,
+};
+
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+    }
+  });
+}, options);
+
+document.querySelectorAll(".card").forEach((card) => {
+  observer.observe(card);
+});
